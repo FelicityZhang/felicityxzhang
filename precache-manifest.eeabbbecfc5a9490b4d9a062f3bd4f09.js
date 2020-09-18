5300f1fd8a381544b57f4f99e2545fb60b921fe1
Felicity <xz1574@nyu.edu>
2020-09-18 13:34:32 -0400

    Updates

---
+++ precache-manifest.eeabbbecfc5a9490b4d9a062f3bd4f09.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e56b51479a3c2f5fffd92c535a1ea9",
+    "url": "/felicityxzhang/index.html"
+  },
+  {
+    "revision": "13b6b6fa40e6d3b94e98",
+    "url": "/felicityxzhang/static/css/main.c4967c61.chunk.css"
+  },
+  {
+    "revision": "29b6e2f2bca3eb938a85",
+    "url": "/felicityxzhang/static/js/2.769fb6f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/felicityxzhang/static/js/2.769fb6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b6b6fa40e6d3b94e98",
+    "url": "/felicityxzhang/static/js/main.6afb00b0.chunk.js"
+  },
+  {
+    "revision": "55f27ff3054dd8bc9de3",
+    "url": "/felicityxzhang/static/js/runtime-main.cf2d4d69.js"
+  },
+  {
+    "revision": "eb3e20769caf6173143c197b993b7bc1",
+    "url": "/felicityxzhang/static/media/Bridge1.eb3e2076.png"
+  },
+  {
+    "revision": "4d51dda677204d0206c5642ff64fac87",
+    "url": "/felicityxzhang/static/media/agent.4d51dda6.png"
+  },
+  {
+    "revision": "07258380ac511a09377680e81671db94",
+    "url": "/felicityxzhang/static/media/chinatown.07258380.png"
+  },
+  {
+    "revision": "89e4b18efc5d2eb9c954df9af5d82245",
+    "url": "/felicityxzhang/static/media/chumely.89e4b18e.png"
+  },
+  {
+    "revision": "ed730cc0b3eaa0d75749726d4ccd0cad",
+    "url": "/felicityxzhang/static/media/email.ed730cc0.png"
+  },
+  {
+    "revision": "e987433d321572ec6abfae98ea0e9aa5",
+    "url": "/felicityxzhang/static/media/gabriel.e987433d.png"
+  },
+  {
+    "revision": "9bfc1b5060a413ac179e5edc68d7ec0f",
+    "url": "/felicityxzhang/static/media/imhere.9bfc1b50.png"
+  },
+  {
+    "revision": "c89c96cf9cc3c21a52a640631e222e51",
+    "url": "/felicityxzhang/static/media/linkedin.c89c96cf.png"
+  },
+  {
+    "revision": "648cb3290cbabe155c7a5a7551093ca5",
+    "url": "/felicityxzhang/static/media/self (4).648cb329.png"
+  },
+  {
+    "revision": "d4349c4d43cdf83b98eb27ba1f49f120",
+    "url": "/felicityxzhang/static/media/shopshop.d4349c4d.png"
+  }
+]);
